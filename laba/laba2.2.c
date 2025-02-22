bc012b7f9510f2619e0772ca679bedb500b06a1b
fathumans <nightvate@gmail.com>
2025-02-22 13:00:50 +1000

    Update laba2.2.c

--- laba/laba2.2.c
+++ laba/laba2.2.c
@@ -1,7 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(int argc, char *argv[]) {
+    int n, Matrix[n][n];
+    printf("Введите числа для матрицы\n");
+    for (int x = 0; x<2; x++){
+        for (int y=0; y<2; y++){
+            printf("Введите элемент [%d] [%d]: ",x,y);
+            scanf("%df", &Matrix[x][y]);
+        }
+    }
+	
 	printf("\nДанный Массив:\n");
 	for (int x = 0; x < 3; x++){
 		for (int y = 0; y < 3; y++){
-			printf("%d ", matrix[x][y]);
+			printf("%d ", Matrix[x][y]);
 		}
 		printf("\n");
-	} 
+	}
+	
+	return 0;
+}
