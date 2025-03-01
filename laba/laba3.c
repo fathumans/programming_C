9a587293767a815f5e1f9f1cbea7d299aa205330
fathumans <nightvate@gmail.com>
2025-03-01 12:21:22 +1000

    Create laba3.c

---
+++ laba/laba3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	char a[81];
+
+	printf("Vvedite stroky simvolov dlini do 80:\n");
+	scanf("%s", &a);
+
+	for (int i = 0; i<81; i++) {
+		if (a[i] == 'a') a[i] = 'A';
+		if (a[i] == 'b') a[i] = 'B';
+	}
+	
+    printf("Vi vveli: %s \n", a);
+	return 0;
+}
