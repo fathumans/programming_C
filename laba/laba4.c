037106454e7e56239d77a01e080fe336f05e242f
fathumans <nightvate@gmail.com>
2025-03-15 13:04:45 +1000

    Update laba4.c

--- laba/laba4.c
+++ laba/laba4.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
-#include <math.h>
-
-int Perimetr(double a, double b,double c){
-    return a + b + c;
-};
-
-int Ploshad(double a, double b, double c, double p){
-    return sqrt(p*(p-a)*(p-b)*(p-c));
-};
+#include "Triangle.h"
 
 int main(int argc, char *argv[])
 {
@@ -21,8 +13,9 @@ int main(int argc, char *argv[])
 	scanf("%lf", &c);
 
 
-	if ((a < b+c) && (b <c+a) && (c < a+b)) {
+	if (RealorFake(a,b,c)) {
         double CurS, CurP;
+        
         CurP = Perimetr(a,b,c);
         CurS = Ploshad(a, b, c, CurP/2);
         
