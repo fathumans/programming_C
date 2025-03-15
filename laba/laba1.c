d8d3619459f1b1c9d987602fd7262c46feaafae2
fathumans <nightvate@gmail.com>
2025-03-15 12:07:07 +1000

    Update laba1.c

--- laba/laba1.c
+++ laba/laba1.c
@@ -13,30 +13,34 @@
 int main(int argc, char *argv[]) {
 	double a,b,c;
 	double x1,x2,d;
-	printf("Vvedite a:\n");
-	scanf("%f", &a);
-	printf("Vvedite b:\n");
-	scanf("%f", &b);
-	printf("Vvedite c:\n");
-	scanf("%f", &c);
 	
-	d = pow(b,2) - 4*a*c;
-	printf("D=%1f\n", d);
-	
-	
-	if (d<0) printf("Koreny Net");
-	
-	if (d>0) {
-		x1= (-b + sqrt(d))/(2*a);
-		x2= (-b - sqrt(d))/(2*a);
-		
-		printf("x1=%1d\n", x1);
-		printf("x2=%1d\n", x2);
+	while(1){
+	    printf("Vvedite a:\n");
+    	scanf("%lf", &a);
+    	printf("Vvedite b:\n");
+    	scanf("%lf", &b);
+    	printf("Vvedite c:\n");
+    	scanf("%lf", &c);
+    	
+    	d = pow(b,2) - 4*a*c;
+    	printf("D=%lf\n", d);
+    	
+    	
+    	if (d<0) printf("Koreny Net\n");
+    	
+    	if (d>0) {
+    		x1= (-b + sqrt(d))/(2*a);
+    		x2= (-b - sqrt(d))/(2*a);
+    		
+    		printf("x1=%lf\n", x1);
+    		printf("x2=%lf\n", x2);
+    	}
+    	if (d==0) {
+    		x1 = (-b + sqrt(d))/(2*a);
+    		printf("Only one root x = %lf\n", x1);
+    	}
 	}
-	if (d==0) {
-		x1 = (-b + sqrt(d))/(2*a);
-		printf("Only one root x = %1d\n", x1);
-	}
+	
 	
 	return 0;
 }
