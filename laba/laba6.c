b021d29a69ef163dbedb15de91c4af2689bd4340
fathumans <nightvate@gmail.com>
2025-03-22 13:15:40 +1000

    Update laba6.c

--- laba/laba6.c
+++ laba/laba6.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#define Max_Name_Length 100
 
+int main()
+{
+    File
+    
+    char Familia[Max_Name_Length];
+    char Imya[Max_Name_Length];
+    char Otchevstvo[Max_Name_Length];
+    int Year;
+    
+    while (fscanf(file, "%s %s %s %d", Familia, Imya, Otchevstvo , Year) == 4) {
+        if year > 1980 {
+            fprintf(outputfile, "%s %s %s %d", Familia, Imya, Otchevstvo, Year)
+        }
+    }
+
+
+
+    return 0;
+}
