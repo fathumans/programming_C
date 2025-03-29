dc2f756ff2b2faf1116e60101d58abe4b08ea4e6
fathumans <nightvate@gmail.com>
2025-03-29 12:07:20 +1000

    Update laba6.c

--- laba/laba6.c
+++ laba/laba6.c
@@ -4,19 +4,18 @@
 
 int main()
 {
-    File
+    FILE *f1 = fopen("in.txt", "r");
+    FILE *f2 = fopen("out.txt", "w");
     
     char Familia[Max_Name_Length];
     char Imya[Max_Name_Length];
     char Otchevstvo[Max_Name_Length];
     int Year;
     
-    while (fscanf(file, "%s %s %s %d", Familia, Imya, Otchevstvo , Year) == 4) {
-        if year > 1980 {
-            fprintf(outputfile, "%s %s %s %d", Familia, Imya, Otchevstvo, Year)
-        }
-    }
-
+    while (fscanf(f1, "%s %s %s %d", Familia, Imya, Otchevstvo , &Year) == 4) {
+        
+        if (Year > 1980) fprintf(f2, "%s %s %s %d\n", Familia, Imya, Otchevstvo, Year);
+ } 
 
 
     return 0;
