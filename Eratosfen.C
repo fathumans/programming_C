22c900a7d968f1562a4bdc31eaa30a84eeed24ae
fathumans <nightvate@gmail.com>
2025-06-07 11:37:15 +1000

    Update Eratosfen.C

--- Eratosfen.C
+++ Eratosfen.C
@@ -25,3 +25,29 @@ int main(int argc, char *argv[]) {
     
     return 0;
 }
+
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    time_t now;
+    time(&now);
+    struct tm *tm_now = localtime(&now);
+    
+    // Создаем структуру для 1 января следующего года
+    struct tm new_year = *tm_now;
+    new_year.tm_year += 1;
+    new_year.tm_mon = 0;
+    new_year.tm_mday = 1;
+    new_year.tm_hour = 0;
+    new_year.tm_min = 0;
+    new_year.tm_sec = 0;
+    
+    time_t new_year_time = mktime(&new_year);
+    double diff = difftime(new_year_time, now);
+    int days_left = (int)(diff / (60 * 60 * 24));
+    
+    printf("До начала %d года осталось %d дней.\n", new_year.tm_year + 1900, days_left);
+    return 0;
+}
+
